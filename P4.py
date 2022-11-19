baa0d020b85a739469956be0cf985789f26a3959
LoBot1 <logan.bnt4@gmail.com>
2022-11-20 00:22:11 +0100

    feature : table p4

---
+++ P4.py
@@ -0,0 +1,19 @@
+from pygame import *
+
+
+# def equal(player1,player2,p4):
+#     coup_win = 0
+#     if coup_win == 4 :
+#         print("win")
+        
+
+
+
+P4 =[(0,0,0,0,0,0,0), 
+     (0,0,0,0,0,0,0),
+     (0,0,0,0,0,0,0),
+     (0,0,0,0,0,0,0),
+     (0,0,0,0,0,0,0),
+     (0,0,0,0,0,0,0)]
+for i in P4 :
+    print (i) 
