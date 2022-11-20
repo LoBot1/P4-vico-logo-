48cb58fefcb8efe34652ae90092d5cf22e4f612f
LoBot1 <logan.bnt4@gmail.com>
2022-11-20 22:43:15 +0100

    feature : menu + mise en def de table p4

--- P4.py
+++ P4.py
@@ -8,12 +8,44 @@ from pygame import *
         
 
 
+def table():
+     print("salut joueur")
+     P4 =[(0,0,0,0,0,0,0), 
+          (0,0,0,0,0,0,0),
+          (0,0,0,0,0,0,0),
+          (0,0,0,0,0,0,0),
+          (0,0,0,0,0,0,0),
+          (0,0,0,0,0,0,0)]
+     for i in P4 :
+          print (i) 
 
-P4 =[(0,0,0,0,0,0,0), 
-     (0,0,0,0,0,0,0),
-     (0,0,0,0,0,0,0),
-     (0,0,0,0,0,0,0),
-     (0,0,0,0,0,0,0),
-     (0,0,0,0,0,0,0)]
-for i in P4 :
-    print (i) 
+def menu():
+         
+     choix = input("do you want play ('yes' or 'no'):")
+     if choix == 'yes':
+          return table()
+     elif choix == "non":
+          print ("np see you next") 
+     else : 
+          print("you have to answer 'yes' or 'non'") + menu()     
+menu()
+     
+
+     # def table():
+#      input("do you want play :")
+#      if input == "yes":
+#           return 
+#           P4 =[(0,0,0,0,0,0,0), 
+#      (0,0,0,0,0,0,0),
+#      (0,0,0,0,0,0,0),
+#      (0,0,0,0,0,0,0),
+#      (0,0,0,0,0,0,0),
+#      (0,0,0,0,0,0,0)]
+#           for i in P4 :
+#                print (i) 
+#      elif input == "no":
+#           return "okay np see you next"
+#      else :
+#           print ("error") + table()
+          
+# table() 
